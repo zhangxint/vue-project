ab05e70c69c9b35f7ca0f4f79c836f98683de31c
zhangxint <461815052@qq.com>
2021-09-16 11:25:23 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" @click="clickAction">
+      <p class="ps">Accelerated & hosted by 4EVERLAND</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  methods: {
+      clickAction(){
+          window.location.href = 'https://hosting.4everland.org/#/?invite=R41Y6IPN'
+      },
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    position:fixed;
+    top: 0;
+    left: 0;
+    width:100%;
+    height:100%;
+    z-index:-10;
+    zoom: 1;
+    background: url('./assets/bg.jpg');
+    background-color: #fff;
+    background-repeat: no-repeat;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-position: center 0;
+    cursor: pointer;
+}
+.ps{
+    position: fixed;
+    bottom: 5%;
+    right: 5%;
+    font-size: 20px;
+    color: #fff;
+    font-weight: bolder;
 }
 </style>
